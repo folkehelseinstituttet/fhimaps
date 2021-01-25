751ba3f365c5109172009cadbbb22ccc2cd74b2f
Richard Aubrey White <hello@rwhite.no>
2021-01-25 06:36:28 +0100

    Cleaning up attach message

--- R/1_onAttach.R
+++ R/1_onAttach.R
@@ -1,9 +1,7 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0(
-    "\033[40m\033[37m",
     "fhimaps ",
     utils::packageDescription("fhimaps")$Version,
-    "\033[39m\033[49m ",
     "https://folkehelseinstituttet.github.io/fhimaps","\033[39m"
   ))
 }
