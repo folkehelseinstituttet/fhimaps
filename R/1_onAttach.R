11b56857b0c022211269cda53e5032aa73694634
Richard Aubrey White <hello@rwhite.no>
2021-01-25 06:34:49 +0100

    Final touches on cleaning up

--- R/1_onAttach.R
+++ R/1_onAttach.R
@@ -1,7 +1,9 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0(
-    "\033[32m","fhimaps ","\033[39m",
-    "\033[30m",utils::packageDescription("fhimaps")$Version,"\033[39m",
-    "\033[35m"," https://folkehelseinstituttet.github.io/fhimaps","\033[39m"
-    ))
+    "\033[40m\033[37m",
+    "fhimaps ",
+    utils::packageDescription("fhimaps")$Version,
+    "\033[39m\033[49m ",
+    "https://folkehelseinstituttet.github.io/fhimaps","\033[39m"
+  ))
 }
